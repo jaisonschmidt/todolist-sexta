c4e9a1434909f3ac35a7e4ec0690bc2fa69e3ca0
Jaison <jaisonschmidt@gmail.com>
2023-09-22 20:49:10 -0300

    feat: cria função renderTasks

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
 // armazena todas as tarefas cadastradas
 const tasks = [];
+const taskListElement = document.getElementById("taskListElement");
+const emptyTaskListElement = document.getElementById("emptyTaskListElement");
 
 function newId() {
     return Math.floor(Math.random() * 1000); // Gerar IDs entre 0 e 999
@@ -16,3 +18,43 @@ function createTask(taskTitle, taskDescription = "") {
     // renderTasks();
     return task;
 }
+
+function renderTasks(listElement = taskListElement, emptyMessage = emptyTaskListElement) {
+    let finalHtml = "";
+
+    if (tasks.length === 0) {
+        emptyMessage.style.display = 'block';
+        listElement.style.display = 'none';
+        return;
+    }
+
+    emptyMessage.style.display = 'none';
+    listElement.style.display = 'flex';
+
+    tasks.forEach(function(item){
+        finalHtml += `
+            <details class="taskItem">
+                <summary class="taskTitle">${item.taskTitle}</summary>
+                ${item.taskDescription}
+                <div>
+                    <button 
+                        class="btn" 
+                        onclick="if (confirm('Tem certeza?')) deleteTask(${item.id})"
+                    >
+                        Pronto
+                    </button>
+                    <button 
+                        class="btn btn-info"
+                        onclick="renderFormUpdate(${item.id})"
+                    >
+                        Alterar
+                    </button>
+                </div>
+            </details>
+        `;
+    });
+   
+    listElement.innerHTML = finalHtml;
+}
+
+renderTasks();
